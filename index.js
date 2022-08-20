a63c07a71b3f3f3aabfe271e08a526f6e84bcb5d
Francis <mufraggie@gmail.com>
2022-08-20 11:55:51 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin (record){
+    const result = record.find(element => element.result==='W');
+    return result !== undefined ? result.year : result;
+}
